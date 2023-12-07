e9d7b76582ee41f5730a3140de86d0befb45587b
ofofonon <ofinaboy1@gmail.com>
2023-12-07 06:24:06 -0800

    biky
    bike

---
+++ virtual.js
@@ -0,0 +1,11 @@
+
+const choose=document.getElementById('ch');
+const elec=document.getElementById('elc');
+
+
+function showelec(){
+   choose.style.display='none';
+   elec.style.display='block'
+
+    
+}
